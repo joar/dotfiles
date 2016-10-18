6c6c0bee007923526eb278f8f6f40aa03d746467
Joar Wandborg <joar@wandborg.se>
2016-10-18 13:53:19 +0200

    [vim] Add fzf path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,18 @@
 set -e fish_greeting
 
-# rustup/cargo bin
-if test -d ~/.cargo/bin
-    set -gx PATH ~/.cargo/bin $PATH
+function __fish_config_optional_path \
+        --argument-names path \
+        --description 'Add a path to $PATH if it exists'
+    if test -d $path
+        set -gx PATH $path $PATH
+    end
 end
+
+# rustup/cargo bin
+__fish_config_optional_path ~/.cargo/bin
+
+# fzf
+__fish_config_optional_path ~/.fzf/bin
 
 set -x __fish_config_dir (dirname (status -f))
 set -l __local_config $__fish_config_dir/config.local.fish
