9aa996a95939693bd9895a1f69227cfd92ac4f1b
Joar Wandborg <joar@wandborg.se>
2016-11-15 15:21:32 +0100

    Bugfix pt-by-fileext

--- .config/fish/functions/pt-by-fileext.fish
+++ .config/fish/functions/pt-by-fileext.fish
@@ -1,13 +1,15 @@
 function pt-by-fileext --description 'Run \'pt\' with --file-search-regexp set to $fileext' --argument-names search fileext
-  set -l pt_args $argv[3..-1]
-  echo pt_args $pt_args
+  set -l pt_args
+
+  if test (count $argv) -gt 2
+    set pt_args $argv
+  end
 
   if not test -z "$fileext"
     set pt_args $pt_args \
       --file-search-regexp='\.('(string join '|' (string split ',' $fileext))')$'
   end
 
-  echo pt $pt_args $search
   pt $pt_args $search
 end
 
