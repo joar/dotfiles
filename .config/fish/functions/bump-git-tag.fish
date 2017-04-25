1213ff35f9dcdafc9ecf7f09967d4a8c38c5493f
Joar Wandborg <joar@wandborg.se>
2017-04-25 14:42:14 +0200

    Update bump-git-tag

--- .config/fish/functions/bump-git-tag.fish
+++ .config/fish/functions/bump-git-tag.fish
@@ -1,23 +1,18 @@
 function bump-git-tag \
     --description 'Bump the git-tagged version' \
-    --argument-names semver_increment semver_preid
+    --argument-names semver_increment semver_preid current_version
     set -l versions (semver (git tag))
     set -l autobumped_version
     set -l right_prompt
 
-    if test -n "$versions"
-        set -l current_version $versions[1]
+    if test -z "$current_version"
+        and test -n "$versions"
+        set current_version $versions[-1]
+    end
 
+    if test -n "$versions"
         if test -z "$semver_increment"
             set semver_increment 'prerelease'
-        end
-
-        set -l semver_input $current_version
-
-        if test "$semver_increment" = 'prerelease'
-            and not echo $semver_input | sed -n '/-./q 0; q 1'
-            # avoid bumping patch when creating a new prerelease
-            set semver_input $semver_input'-fake'
         end
 
         set -l semver_args --increment $semver_increment
@@ -26,7 +21,7 @@ function bump-git-tag \
             set semver_args $semver_args --preid $semver_preid
         end
 
-        set autobumped_version (semver $semver_args $semver_input)
+        set autobumped_version (semver $semver_args $current_version)
         set right_prompt (set_color blue; echo autobumped from version: $current_version; set_color normal)
     end
 
